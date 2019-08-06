70b6752b2c46c27f019301dcdbad06131b547e53
Steve George <steve@pagerange.com>
2019-08-06 18:40:06 -0500

    Put all code in index.php

---
+++ cache/154064dde299e949e3e21a949e77dfe425e467cd.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="" />
+    <title><?php echo e($name); ?></title>
+</head>
+<body>
+
+<h1>Hello, <?php echo e($name); ?>!</h1>
+
+
+</body>
+</html><?php /**PATH C:\Users\sgeorge\Desktop\blade_test\views/index.blade.php ENDPATH**/ ?>
--- public/index.php
+++ public/index.php
@@ -2,8 +2,17 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-require __DIR__ . '/../functions.php';
+use Philo\Blade\Blade;
+
+// where are the views stored?
+$views = __DIR__ . '/../views';
+
+// where are cached views stored?
+$cache = __DIR__ . '/../cache';
+
+// Create a new blade object
+$blade = new Blade($views, $cache);
 
 $name = 'Steve';
-
-return view('index', compact('name'));
+ 
+echo $blade->view()->make( 'index', compact('name') )->render();
